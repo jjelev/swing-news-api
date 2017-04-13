3b4080bb5be595cf52015f1488a3ab5261b0b025
Jeliu Jelev <j.jelev@yandex.com>
2017-04-13 23:45:36 +0300

    News Controller template

---
+++ src/Controllers/News.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Swing\Controllers;
+
+class News
+{
+    public function get()
+    {
+
+    }
+
+    public function set()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
