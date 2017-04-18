a6cc22ec510b2fe56e2cf3f9de4a12798fa93bb3
Jeliu Jelev <j.jelev@yandex.com>
2017-04-18 20:10:33 +0300

    Basic Request functionality

--- src/Request.php
+++ src/Request.php
@@ -4,5 +4,38 @@ namespace Swing;
 
 class Request
 {
+    private $request;
 
+    function __construct()
+    {
+        $this->request = $_REQUEST;
+    }
+
+    public function all()
+    {
+        return $this->request;
+    }
+
+    public function input(?string $key = null, ?string $default = null)
+    {
+        if ($key === null) {
+            return $this->request;
+        }
+
+        if (array_key_exists($key, $this->request)) {
+            return $this->request[$key];
+        }
+
+        return $default;
+    }
+
+    public function method()
+    {
+        return $_SERVER['REQUEST_METHOD'];
+    }
+
+    public function isMethod(string $method)
+    {
+        return strtoupper($method) === $this->method();
+    }
 }
