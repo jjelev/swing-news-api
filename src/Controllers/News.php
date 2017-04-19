2edbb49760b4450962de1c6f37e46732b6311c93
Jeliu Jelev <j.jelev@yandex.com>
2017-04-19 23:52:06 +0300

    Controller Actions

--- src/Controllers/News.php
+++ src/Controllers/News.php
@@ -2,6 +2,7 @@
 
 namespace Swing\Controllers;
 
+use PDOException;
 use Swing\Models\News as Model;
 use Swing\Request;
 use Swing\Response;
@@ -18,34 +19,77 @@ class News
         $this->db = new $model;
     }
 
-    public function get(Request $request, ?int $id = null)
+    public function get(?int $id = null)
     {
         //TODO: Validation!
 
         try {
-            $result = $this->db->select($id);
+            $content = $this->db->select($id);
 
-        } catch (\PDOException $e) {
-            return $e->getMessage();
+        } catch (PDOException $e) {
+            $content = ['errors' => [$e->getMessage()]];
         }
 
-        $response = new Response($result);
+        $response = new Response($content);
 
         $response->json();
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        $title = $request->input('title');
+        $text = $request->input('text');
 
+        try {
+            $result = $this->db->insert($title, $text);
+            $content = (['result' => $result]);
+
+        } catch (PDOException $e) {
+            $content = ['errors' => [$e->getMessage()]];
+        }
+
+        $response = new Response();
+
+        $response->setContent($content);
+
+        $response->json();
     }
 
-    public function update(int $id)
+    public function update(Request $request, int $id)
     {
+        $title = $request->input('title');
+        $text = $request->input('text');
+        $date = $request->input('date');
 
+        try {
+            $result = $this->db->update($id, $title, $text, $date);
+            $content = (['result' => $result]);
+
+        } catch (PDOException $e) {
+            $content = ['errors' => [$e->getMessage()]];
+        }
+
+        $response = new Response();
+
+        $response->setContent($content);
+
+        $response->json();
     }
 
-    public function delete()
+    public function delete(int $id)
     {
+        try {
+            $result = $this->db->delete($id);
+            $content = (['result' => $result]);
 
+        } catch (PDOException $e) {
+            $content = ['errors' => [$e->getMessage()]];
+        }
+
+        $response = new Response();
+
+        $response->setContent($content);
+
+        $response->json();
     }
 }
