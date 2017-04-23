c868bace2f739a31464c54381ed331076fb51e20
Jeliu Jelev <j.jelev@yandex.com>
2017-04-23 04:02:00 +0300

    Router Test

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -18,7 +18,11 @@ class RouterTest extends TestCase
     public function testRouterMatchingOnGet()
     {
         $router = new Router('GET', '/News/1');
+
+        $router->map('POST', '/news/:id', 'News#update');
+        $router->map('POST', '/news', 'News#create');
         $router->map('GET', '/news/[:id]', ['News', 'get']);
+        $router->map('DELETE', '/news/:id', 'News#delete');
 
         $actualRoute = [
             'method' => 'GET',
