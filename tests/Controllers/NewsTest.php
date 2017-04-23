7c3942c18894a65f5a259412ad92bbbea88a29e9
Jeliu Jelev <j.jelev@yandex.com>
2017-04-23 23:01:07 +0300

    Controller and Model Tests

---
+++ tests/Controllers/NewsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Swing\Tests\Controllers;
+
+use PHPUnit\Framework\TestCase;
+
+class NewsTest extends TestCase
+{
+    public function testGetAll()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testGetSingle()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testCreate()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testUpdate()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testDelete()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Models/NewsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Swing\Models;
+
+use PHPUnit\Framework\TestCase;
+
+class NewsTest extends TestCase
+{
+    public function testSelect()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testInsert()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testUpdate()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testDelete()
+    {
+        $this->assertTrue(true);
+    }
+}
