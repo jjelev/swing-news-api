97628383e243e4426f0d42dc9469237ce1d09b28
Jeliu Jelev <j.jelev@yandex.com>
2017-04-23 22:53:28 +0300

    Namespaces Fix

--- public/index.php
+++ public/index.php
@@ -3,13 +3,13 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 try {
-    (new Dotenv\Dotenv(__DIR__ . '/../'))->load();
-} catch (Dotenv\Exception\InvalidPathException $e) {
+    (new \Dotenv\Dotenv(__DIR__ . '/../'))->load();
+} catch (\Dotenv\Exception\InvalidPathException $e) {
     // In case somebody forgets to put .env in root directory.
     echo $e->getMessage();
     exit;
 }
 
-$kernel = new Swing\Kernel();
+$kernel = new \Swing\Kernel();
 
 $kernel->run();
