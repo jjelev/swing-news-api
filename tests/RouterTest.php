eb4514e67d9a510893a522e007f694ae55917de5
Jeliu Jelev <j.jelev@yandex.com>
2017-04-23 03:59:07 +0300

    Router Test

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -1,9 +1,32 @@
 <?php
 
-use Swing\Router;
+namespace Swing\Tests;
+
 use PHPUnit\Framework\TestCase;
+use Swing\Router;
 
 class RouterTest extends TestCase
 {
+    public function testRouterMatchingNonExistent()
+    {
+        $router = new Router('GET', '/nonexistent');
+        $router->map('GET', '/news/[:id]', ['News', 'get']);
 
+        $this->assertFalse($router->match());
+    }
+
+    public function testRouterMatchingOnGet()
+    {
+        $router = new Router('GET', '/News/1');
+        $router->map('GET', '/news/[:id]', ['News', 'get']);
+
+        $actualRoute = [
+            'method' => 'GET',
+            'path' => ['news', '[:id]'],
+            'action' => ['News', 'get'],
+            'url_path' => ['news', '1'],
+        ];
+
+        $this->assertEquals($router->match(), $actualRoute);
+    }
 }
